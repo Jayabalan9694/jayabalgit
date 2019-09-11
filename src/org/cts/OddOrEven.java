9a3b8d83365eb3b59fcfea8112548533ca4c89b0
Jayabalan9694 <jayabal9694@gmail.com>
2019-09-11 19:28:27 +0530

    my Java

---
+++ src/org/cts/OddOrEven.java
@@ -0,0 +1,24 @@
+package org.cts;
+
+public class OddOrEven {
+	public static void main(String[] args) {
+		 
+				
+				for(int i=1;i<=100;i++) {
+					
+					if(i%2==0) {
+						System.out.println("i am even"+i);
+					}
+					else {
+						System.out.println( "odd is"+i);
+						
+					}
+					
+					
+				}
+				
+			}
+			
+	
+
+}
